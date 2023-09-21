fb177503426d9a8a743cd5ae196ec7ec9991d60e
Gabrillos <gabrielluizdacruzignacio@gmail.com>
2023-09-21 14:23:37 -0300

    Commit inicial

--- App.js
+++ App.js
@@ -1,15 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const[username, setUsername] = useState('')
+  const[password, setPassword] = useState('')
+  const[passwordConfirm, setPasswordConfirm] = useState('')
 
+
+function handleSignIn(){ 
+  const data = {
+    username,
+    password, 
+    passwordConfirm,
+  }
+  console.log(data);
+  }
+
+return(
+  <View style={styles.container}>
+
+      <text style={styles.title}>Cadastrar</text>
+
+      <TextInput
+      style={style.input}
+      onChangeText={setUsername}
+      value={username}
+      placeholder="Nome de usuario"/>
+
+      <TextInput
+        style={style.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Digite uma senha"
+        secureTextEntry={true}/>
+
+      <TextInput
+        style={style.input}
+        onChangeText={setPasswordConfirm}
+        value={passwordConfirm}
+        placeholder="Confirme sua senha"
+        secureTextEntry={true}/>
+
+
+
+
+
+
+
+
+
+
+
+  </View>
+)}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
